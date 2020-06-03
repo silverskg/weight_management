c93ebdba39b8433c96f510b95b0f6b0cea9970c2
silverskg <chokichoki_gm@i.softbank.jp>
2020-06-03 20:47:34 +0900

    index.html. 編集

--- app/assets/javascripts/graphs.js
+++ app/assets/javascripts/graphs.js
@@ -4,21 +4,32 @@ document.addEventListener('turbolinks:load', () => {
     
     // 日付の古い方・新しい方を取得する関数
     const minDate = (date1, date2) => (date1 < date2) ? date1 : date2
-    const maxDate = (date1, date2) => (date1 < date2) ? date1 : date2
+    const maxDate = (date1, date2) => (date1 > date2) ? date1 : date2
     const convertDate = (date) => new Date(new Date(date).setHours(0, 0, 0, 0))
     // データの初日・最終日
     const START_DATE = convertDate(gon.weight_records[0].date)
     const END_DATE = convertDate(gon.weight_records[gon.weight_records.length - 1].date)
     
     // カレンダー日本語化
-    flatpickr.localize(flatpickr.l10ns.js)
+    flatpickr.localize(flatpickr.l10ns.ja)
+    
+    const drawGraphForPeriod = () => {
+        let from = convertDate(document.getElementById('start-calendar').value)
+        let to = convertDate(document.getElementById('end-calendar').value)
 
+        if (from > to) {
+            alert('終了日は開始日以降の日付に設定して下さい')
+        } else {
+            drawGraph(from, to)
+        }
+    }
     const periodCalendarOption = {
       // スマホでもカレンダーflatpickrを使用
-      disableMobile: true
+      disableMobile: true,
       // 選択できる期間
       minDate: START_DATE,
       maxDate: END_DATE,
+      onChange: drawGraphForPeriod
     }
 
     // カレンダー
@@ -35,9 +46,9 @@ document.addEventListener('turbolinks:load', () => {
     const chartWeightContext = document.getElementById("chart-weight").getContext('2d')
 
     let chartWeight
+    
+
     // 期間を指定してグラフを描く
-
-
     const drawGraph = (from, to) => {
         // from から to までの期間のデータに絞る
         let records = gon.weight_records.filter((record) => {
@@ -80,26 +91,31 @@ document.addEventListener('turbolinks:load', () => {
                 }
             }
         }
-
-        if (!chartWeight){
-           // グラフが存在しないときは，作成する
+        
+        if (!chartWeight) {
+          // グラフが存在しないときは，作成する
           chartWeight = new Chart(chartWeightContext, {
-            type: 'line',
-            data: weightData,
-            options: weightOption
+              type: 'line',
+              data: weightData,
+              options: weightOption
           })
-        } else {
+      } else {
           // グラフが存在するときは，更新する
           chartWeight.data = weightData
           chartWeight.options = weightOption
           chartWeight.update()
-        }
+      }
+  }
       // 引数の日付から今日までのグラフを描く関数
     const drawGraphToToday = (from) => {
       // データが存在する範囲に修正
       from = maxDate(from, START_DATE)
       let to = minDate(TODAY, END_DATE)
       drawGraph(from, to)
+
+      //　フォームの開始日・終了日を変更する
+      startCalendarFlatpickr.setDate(from)
+      endCalendarFlatpickr.setDate(to)
   }
 
   // 過去◯週間のグラフを描くボタン
@@ -118,8 +134,8 @@ document.addEventListener('turbolinks:load', () => {
   document.getElementById('three-months-button').addEventListener('click', () => {
       drawGraphToToday(THREE_MONTHS_AGO)
   })
-    }
+  
 
     // グラフの初期表示
-    drawGraph(A_WEEK_AGO, TODAY)
+    drawGraphToToday(A_WEEK_AGO)
 })
